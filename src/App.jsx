493cf32be3f4a03efb1072ee40c5453f73382142
Ozkan Metin <ometin@gmail.com>
2023-07-31 13:56:07 -0700

    adding changes

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import ContactList from './components/ContactList'
+import SelectedContact from './components/SelectedContact';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [selectedContactId, setSelectedContactId] = useState(null);
+
+  return (
+    <>
+      {selectedContactId ? (
+        <div>Selected Contact View</div>
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId}/>
+      )}
+    </>
+  )
+}
+
+export default App
